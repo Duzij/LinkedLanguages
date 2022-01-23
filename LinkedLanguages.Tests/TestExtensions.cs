0a1bec9bfc3d1a8d091f6792513f0f6ec4a37aa2
Maxim Dužij <maxim.duzij@riganti.cz>
2022-01-23 16:57:20 +0100

    PumpPerformedWhenAllWordsRejected test added

--- LinkedLanguages.Tests/TestExtensions.cs
+++ LinkedLanguages.Tests/TestExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Caching.Memory;
 
+using Moq;
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,21 @@ using System.Threading.Tasks;
 
 namespace LinkedLanguages.Tests
 {
+    public static class MockMemoryCacheService
+    {
+        public static IMemoryCache GetMemoryCache()
+        {
+            var memoryCache = Mock.Of<IMemoryCache>();
+            var cachEntry = Mock.Of<ICacheEntry>();
+
+            var mockMemoryCache = Mock.Get(memoryCache);
+            mockMemoryCache
+                .Setup(m => m.CreateEntry(It.IsAny<object>()))
+                .Returns(cachEntry);
+            return memoryCache;
+        }
+    }
+
     public static class TestExtensions
     {
         public static TItem Set<TItem>(this IMemoryCache cache, object key, TItem value, MemoryCacheEntryOptions options)
--- LinkedLanguages.Tests/WordPairPumpTests.cs
+++ LinkedLanguages.Tests/WordPairPumpTests.cs
@@ -20,24 +20,16 @@ using System.Threading.Tasks;
 
 namespace LinkedLanguages.Tests
 {
+
     public class WordPairPumpTests
     {
+        public Guid TestUserTest { get; set; } = Guid.Parse("52d742a9-9240-4542-bdfa-64bfe3f979b9");
+
+
         [Test]
         public async Task PumpNotPerformedIfNotNeeded()
         {
-            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            OperationalStoreOptions storeOptions = new OperationalStoreOptions
-            {
-                //populate needed members
-            };
-
-            IOptions<OperationalStoreOptions> operationalStoreOptions = Options.Create(storeOptions);
-
-            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseInMemoryDatabase(databaseName: "TestDbContext")
-            .Options;
-
-            using (var dbContext = new ApplicationDbContext(options, operationalStoreOptions))
+            using (var dbContext = GetNewTestDbContext())
             {
                 var wordPairs = new List<WordPair>
                 {
@@ -53,18 +45,11 @@ namespace LinkedLanguages.Tests
                 dbContext.UnusedWordPairs.AddRange(wordPairs);
                 dbContext.SaveChanges();
 
-                var memoryCache = Mock.Of<IMemoryCache>();
-                var cachEntry = Mock.Of<ICacheEntry>();
-
-                var mockMemoryCache = Mock.Get(memoryCache);
-                mockMemoryCache
-                    .Setup(m => m.CreateEntry(It.IsAny<object>()))
-                    .Returns(cachEntry);
-
                 var appUserProvider = new Mock<IAppUserProvider>();
+                appUserProvider.Setup(a => a.GetUserId()).Returns(TestUserTest);
                 var unusedUserWordPairsQuery = new UnusedUserWordPairsQuery(dbContext, appUserProvider.Object);
 
-                var wordPairPump = new WordPairPump(new SparqlPairsQuery(), memoryCache, unusedUserWordPairsQuery, dbContext);
+                var wordPairPump = new WordPairPump(new SparqlPairsQuery(), MockMemoryCacheService.GetMemoryCache(), unusedUserWordPairsQuery, dbContext);
 
                 await wordPairPump.Pump("eng", "ll");
 
@@ -76,6 +61,48 @@ namespace LinkedLanguages.Tests
         [Test]
         public async Task PumpPerformedWhenDbEmpty()
         {
+            using (var dbContext = GetNewTestDbContext())
+            {
+                var appUserProvider = new Mock<IAppUserProvider>();
+                var unusedUserWordPairsQuery = new UnusedUserWordPairsQuery(dbContext, appUserProvider.Object);
+
+                var wordPairPump = new WordPairPump(new SparqlPairsQuery(), MockMemoryCacheService.GetMemoryCache(), unusedUserWordPairsQuery, dbContext);
+                await wordPairPump.Pump("eng", "ll");
+
+                Assert.AreEqual(3, dbContext.UnusedWordPairs.Count());
+            }
+        }
+
+        [Test]
+        public async Task PumpPerformedWhenAllWordsRejected()
+        {
+            using (var dbContext = GetNewTestDbContext())
+            {
+                var wordPairsToAppUser = new List<WordPairToApplicationUser>
+                {
+                    new WordPairToApplicationUser {
+                        Id = Guid.NewGuid(),
+                        ApplicationUserId = TestUserTest,
+                        Rejected = true,
+                        WordPairId = Guid.NewGuid()
+                    },
+                };
+
+                dbContext.WordPairToApplicationUsers.AddRange(wordPairsToAppUser);
+                dbContext.SaveChanges();
+
+                var appUserProvider = new Mock<IAppUserProvider>();
+                var unusedUserWordPairsQuery = new UnusedUserWordPairsQuery(dbContext, appUserProvider.Object);
+
+                var wordPairPump = new WordPairPump(new SparqlPairsQuery(), MockMemoryCacheService.GetMemoryCache(), unusedUserWordPairsQuery, dbContext);
+                await wordPairPump.Pump("eng", "ll");
+
+                Assert.AreEqual(3, dbContext.UnusedWordPairs.Count());
+            }
+        }
+
+        private ApplicationDbContext GetNewTestDbContext()
+        {
             var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
             OperationalStoreOptions storeOptions = new OperationalStoreOptions
             {
@@ -85,26 +112,10 @@ namespace LinkedLanguages.Tests
             IOptions<OperationalStoreOptions> operationalStoreOptions = Options.Create(storeOptions);
 
             var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseInMemoryDatabase(databaseName: "TestDbContext")
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
             .Options;
 
-            using (var dbContext = new ApplicationDbContext(options, operationalStoreOptions))
-            {
-                var mockCache = new Mock<IMemoryCache>();
-                var appUserProvider = new Mock<IAppUserProvider>();
-                var unusedUserWordPairsQuery = new UnusedUserWordPairsQuery(dbContext, appUserProvider.Object);
-
-                var wordPairPump = new WordPairPump(new SparqlPairsQuery(), mockCache.Object, unusedUserWordPairsQuery, dbContext);
-                await wordPairPump.Pump("eng", "ll");
-
-                Assert.AreEqual(3, dbContext.UnusedWordPairs.Count());
-            }
-        }
-
-        [Test]
-        public void PumpPerformedWhenAllWordsUsed()
-        {
-            
+            return new ApplicationDbContext(options, operationalStoreOptions);
         }
     }
 }
