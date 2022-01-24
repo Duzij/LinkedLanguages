f6bc311f96ba41d3f0c91a3e0e68f61e19f78cde
Maxim Dužij <maxim.duzij@riganti.cz>
2022-01-24 11:21:53 +0100

    Application insigts added

--- src/Startup.cs
+++ src/Startup.cs
@@ -66,6 +66,7 @@ namespace LinkedLanguages
 
             services.AddTransient<IEmailSender, EmailSender>();
             services.Configure<AuthMessageSenderOptions>(Configuration);
+            services.AddApplicationInsightsTelemetry(Configuration["APPINSIGHTS_CONNECTIONSTRING"]);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
