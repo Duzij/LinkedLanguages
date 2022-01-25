66528f7c4073eea3f60fb6b252c4e461cbd7b566
Maxim Dužij <maxim.duzij@riganti.cz>
2022-01-25 22:41:24 +0100

    Unnecessary sections removed, home page text added

--- src/ClientApp/src/components/Home.js
+++ src/ClientApp/src/components/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class Home extends Component {
   static displayName = Home.name;
@@ -6,20 +7,11 @@ export class Home extends Component {
   render () {
     return (
       <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
+            <h1>Welcome!</h1>
+            <p>LinkedLanguages is a web application, which uses linked linguistic data as a database <a href="https://etytree.toolforge.org">EtyTree</a> for in e-learning purposes.</p>
+            <p>Based on evaluation of the web application will try to investigate whether it is possible to learn new words in foreign language based on etymological links between words in foreign language and the word in language user already know.</p>
+            <p>You can start in <Link to="/setup">Setup section</Link> where you can setup your known and unknown language.
+                Later, in <Link to="/learn">Learn section</Link> LinkedLanguages will serve you some word pairs.</p>
       </div>
     );
   }
