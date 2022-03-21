bcbeb9110c1bc85c23bb0150b90088d1bea2d510
Maxim Dužij <maxim.duzij@riganti.cz>
2022-03-21 15:33:58 +0100

    EtyTreeLocal.Fixes project added

---
+++ EtyTreeLocal.Fixes/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
+using System.Text.RegularExpressions;
+
+var path = @"F:\DEV\LinkedLanguages\EtyTreeLocal.Fixes\enwkt-20180301.etymology.bad.ttl";
+
+File.WriteAllLines(
+        @"F:\DEV\LinkedLanguages\EtyTreeLocal.Fixes\enwkt-20180301.etymology.ttl",
+        File.ReadAllLines(path).Select(line => changeLine(line)
+            )
+        , Encoding.UTF8);
+
+static string changeLine(string line)
+{
+    var pattern = @"/wiki/.+ ";
+    if (Regex.IsMatch(line, pattern))
+    {
+        var replace = Regex.Replace(line, pattern, replaceSpacesWithUnderscore);
+        return replace;
+    }
+
+    return line;
+}
+
+static string replaceSpacesWithUnderscore(Match match)
+{
+    return match.Groups[0].Value.Trim() + "_";
+}
