9962f987436ae39b9de774dadd308cc51948d27f
Maxim Duzij <maxim.duzij@hotmail.com>
2022-08-22 13:02:33 +0200

    DamerauLevenshteinDistance version changed

--- DamerauLevenshteinDistance.Console/DamerauLevenshteinCalculator.cs
+++ DamerauLevenshteinDistance.Console/DamerauLevenshteinCalculator.cs
@@ -1,37 +1,40 @@
-﻿namespace DamerauLevenshteinDistance.Console;
+﻿using System;
 
-public static class DamerauLevenshteinCalculator
+namespace DamerauLevenshteinDistance.Console
 {
-    public static int Calc(string a, string b)
+    public static class DamerauLevenshteinCalculator
     {
-        if (String.IsNullOrEmpty(a) && String.IsNullOrEmpty(b))
+        public static int Calc(string a, string b)
         {
-            return 0;
-        }
-        if (String.IsNullOrEmpty(a))
-        {
-            return b.Length;
-        }
-        if (String.IsNullOrEmpty(b))
-        {
-            return a.Length;
-        }
-        int lengthA = a.Length;
-        int lengthB = b.Length;
-        var distances = new int[lengthA + 1, lengthB + 1];
-        for (int i = 0; i <= lengthA; distances[i, 0] = i++) ;
-        for (int j = 0; j <= lengthB; distances[0, j] = j++) ;
-
-        for (int i = 1; i <= lengthA; i++)
-            for (int j = 1; j <= lengthB; j++)
+            if (String.IsNullOrEmpty(a) && String.IsNullOrEmpty(b))
             {
-                int cost = b[j - 1] == a[i - 1] ? 0 : 1;
-                distances[i, j] = Math.Min
-                (
-                    Math.Min(distances[i - 1, j] + 1, distances[i, j - 1] + 1),
-                    distances[i - 1, j - 1] + cost
-                );
+                return 0;
             }
-        return distances[lengthA, lengthB];
+            if (String.IsNullOrEmpty(a))
+            {
+                return b.Length;
+            }
+            if (String.IsNullOrEmpty(b))
+            {
+                return a.Length;
+            }
+            int lengthA = a.Length;
+            int lengthB = b.Length;
+            var distances = new int[lengthA + 1, lengthB + 1];
+            for (int i = 0; i <= lengthA; distances[i, 0] = i++) ;
+            for (int j = 0; j <= lengthB; distances[0, j] = j++) ;
+
+            for (int i = 1; i <= lengthA; i++)
+                for (int j = 1; j <= lengthB; j++)
+                {
+                    int cost = b[j - 1] == a[i - 1] ? 0 : 1;
+                    distances[i, j] = Math.Min
+                    (
+                        Math.Min(distances[i - 1, j] + 1, distances[i, j - 1] + 1),
+                        distances[i - 1, j - 1] + cost
+                    );
+                }
+            return distances[lengthA, lengthB];
+        }
     }
 }
--- DamerauLevenshteinDistance.Console/Program.cs
+++ DamerauLevenshteinDistance.Console/Program.cs
@@ -2,6 +2,8 @@
 
 using DamerauLevenshteinDistance.Console;
 
+using System;
+
 Console.WriteLine($"Word one {args[0]}, word two {args[1]}");
 Console.WriteLine($"Distance is {DamerauLevenshteinCalculator.Calc(args[0], args[1])}.");
 
