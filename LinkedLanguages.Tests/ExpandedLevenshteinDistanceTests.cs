a8bb246cf548c91a56d7dd53d9b66df1364b9538
Maxim Duzij <maxim.duzij@hotmail.com>
2022-08-22 13:03:18 +0200

    Normalization tests added

---
+++ LinkedLanguages.Tests/ExpandedLevenshteinDistanceTests.cs
@@ -0,0 +1,91 @@
+﻿using AnyAscii;
+
+using DamerauLevenshteinDistance.Console;
+
+using LinkedLanguages.BL;
+using LinkedLanguages.DAL;
+using LinkedLanguages.DAL.Models;
+
+using Lucene.Net.Analysis;
+
+using NUnit.Framework;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedLanguages.Tests
+{
+    public class ExpandedLevenshteinDistanceTests
+    {
+        private SparqlPairsQuery sparqlQuery;
+
+        [SetUp]
+        public void Setup()
+        {
+            sparqlQuery = new SparqlPairsQuery(TestServices.GetMoqOptions());
+        }
+
+        [Test]
+        public void PumpEnglishAndLatin()
+        {
+            var results = sparqlQuery.Execute("eng", LanguageSeed.EnglishLanguageId, "rus", LanguageSeed.RussianLangaugeId, 1, 100);
+
+            Console.WriteLine("Before normalization");
+
+            foreach (var item in results.Select(i => new WordPairLevenshteinWrapper(i)).OrderBy(d => d.Distance))
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.WriteLine("After normalization");
+
+            foreach (var item in results.Select(i => new WordPairLevenshteinWrapper(i).RemoveDiacritis()).OrderBy(d => d.Distance))
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+
+    public class WordPairLevenshteinWrapper
+    {
+        public int Distance { get; set; }
+        public WordPairLevenshteinWrapper(WordPair wordPair)
+        {
+            WordPair = wordPair;
+
+            if (wordPair.UnknownWord is not null && wordPair.KnownWord is not null)
+            {
+                Distance = DamerauLevenshteinCalculator.Calc(wordPair.UnknownWord, wordPair.KnownWord);
+            }
+        }
+
+        public WordPairLevenshteinWrapper(WordPair wordPair, WordPair beforeTransliteration) : this(wordPair)
+        {
+            BeforeTransliteration = beforeTransliteration;
+        }
+
+        public WordPair WordPair { get; }
+
+        public WordPair BeforeTransliteration { get; set; }
+
+        public override string ToString()
+        {
+            return $"KnownWord: {WordPair.KnownWord} {BeforeTransliteration?.KnownWord}, UnknownWord: {WordPair.UnknownWord} {BeforeTransliteration?.UnknownWord}, Distance: {Distance}";
+        }
+
+        public WordPairLevenshteinWrapper RemoveDiacritis()
+        {
+            //WordPair.KnownWord = $"{WordPair.KnownWord.RemoveDiacritics()}, (previously {WordPair.KnownWord})";
+            //WordPair.UnknownWord = $"{WordPair.UnknownWord.RemoveDiacritics()}, (previously {WordPair.UnknownWord})";
+            BeforeTransliteration = new WordPair { KnownWord = WordPair.KnownWord, UnknownWord = WordPair.UnknownWord };
+
+            WordPair.KnownWord = $"{WordPair.KnownWord.Transliterate()}";
+            WordPair.UnknownWord = $"{WordPair.UnknownWord.Transliterate()}";
+
+            return new WordPairLevenshteinWrapper(WordPair, BeforeTransliteration);
+        }
+    }
+}
---
+++ LinkedLanguages.Tests/LanguageTests.cs
@@ -0,0 +1,184 @@
+﻿using LinkedLanguages.DAL;
+
+using NUnit.Framework;
+
+using System;
+using System.Globalization;
+using System.Linq;
+
+namespace LinkedLanguages.Tests
+{
+    public class LanguageTests
+    {
+        public string[] EtyTteeLangs { get; }
+
+        public LanguageTests()
+        {
+            EtyTteeLangs = new string[]
+            {
+                "eng",
+                "lat",
+                "en",
+                "deu",
+                "fin",
+                "fra",
+                "ita",
+                "hun",
+                "xcl",
+                "nld",
+                "gem-pro",
+                "rus",
+                "non",
+                "sla-pro",
+                "jpn",
+                "spa",
+                "grc",
+                "gle",
+                "ine-pro",
+                "hbs",
+                "fro",
+                "enm",
+                "ang",
+                "nob",
+                "san",
+                "ara",
+                "swe",
+                "por",
+                "ces",
+                "isl",
+                "epo",
+                "zho",
+                "nno",
+                "fas",
+                "pol",
+                "dan",
+                "goh",
+                "sga",
+                "dum",
+                "mul",
+                "kor",
+                "lav",
+                "tur",
+                "glv",
+                "gla",
+                "smi-pro",
+                "tel",
+                "ron",
+                "gmh",
+                "fiu-fin-pro",
+                "ido",
+                "hye",
+                "vol",
+                "cat",
+                "vie",
+                "ell",
+                "frm",
+                "hin",
+                "tha",
+                "gml",
+                "kat",
+                "heb",
+                "odt",
+                "ll",
+                "fao",
+                "ofs",
+                "osx",
+                "roa-opt",
+                "slk",
+                "trk-pro",
+                "cel-pro",
+                "nci",
+                "msa",
+                "la-vul",
+                "orv",
+                "lit",
+                "sqi",
+                "ceb",
+                "nrf",
+                "gmq-osw",
+                "ota",
+                "tgl",
+                "cym",
+                "est",
+                "yid",
+                "nav",
+                "ltz",
+                "poz-pro",
+                "egy",
+                "ml",
+                "glg",
+                "urj-pro",
+                "dsb",
+                "iir-pro",
+                "vep",
+                "got",
+                "cel-bry-pro",
+                "ira-pro",
+                "frk",
+                "pal",
+                "itc-pro",
+                "ine-bsl-pro",
+                "chu",
+                "mon",
+                "pro",
+                "ind",
+                "sem-pro",
+                "rup",
+                "alg-pro",
+                "khm",
+                "sqj-pro",
+                "osp",
+                "sme",
+                "mga",
+                "sit-pro",
+                "kur",
+                "gmq-bot",
+                "tai-pro",
+                "ltc",
+                "mya",
+                "swa",
+                "la-new",
+                "xgn-pro",
+                "poz-mly-pro",
+                "sco",
+                "bnt-pro",
+                "nds-de",
+                "pli",
+                "bel",
+                "map-pro",
+                "psu",
+                "syc",
+                "xno",
+                "lao",
+                "bod",
+                "slv",
+                "jbo",
+                "poz-pol-pro",
+                "nds"
+            };
+        }
+
+        [Test]
+        public void RunAllTests()
+        {
+            var allLangs = CultureInfo
+                    .GetCultures(CultureTypes.NeutralCultures);
+
+            foreach (var item in EtyTteeLangs)
+            {
+                var lang = allLangs
+                    .Where(ci => string.Equals(ci.ThreeLetterISOLanguageName, item, StringComparison.OrdinalIgnoreCase))
+                    .FirstOrDefault();
+
+                if (lang != null)
+                { 
+                    Console.WriteLine($"{lang.EnglishName}, {lang.NativeName}");
+                }
+                else
+                {
+                    Console.WriteLine($"{item} NOT FOUND");
+                }
+            }
+        }
+    }
+}
