bfb5abb653d6dcbf24324cdec3871e4fe4f308f3
Maxim Duzij <maxim.duzij@hotmail.com>
2022-10-18 22:12:42 +0200

    Link imported

--- src/LinkedLanguages/ClientApp/src/components/pages/Home.js
+++ src/LinkedLanguages/ClientApp/src/components/pages/Home.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component} from 'react';
+import {Link} from 'react-router-dom'
 
 export class Home extends Component {
   static displayName = Home.name;
