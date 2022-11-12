97366c91dd57b097c1794432de2957d2a1a62772
Maxim Duzij <maxim.duzij@hotmail.com>
2022-11-12 23:03:48 +0100

    CLLD tests added

---
+++ src/LinkedLanguages.Tests/LevenshteinDistanceTests/CLLDTests.cs
@@ -0,0 +1,27 @@
+﻿using LinkedLanguages.BL.CLLD;
+
+using NUnit.Framework;
+
+using System;
+
+namespace LinkedLanguages.Tests.LevenshteinDistanceTests
+{
+    public class CLLDTests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+
+        }
+
+
+        [Test]
+        public void NullCharactersMappingTest()
+        {
+            int distance = CrossLingualLevenshteinDistanceCalculator.Calc("Goldschmidt", "Schmidt", GermanCharacterMapper.EnglishToGermanMapping);
+            Console.WriteLine(distance);
+        }
+
+    }
+}
